4d83902bea3f7c5a7d3bb780eff4b8013ed71998
egor <ega.nik5674@gmail.com>
2024-08-10 13:50:50 +0300

    Update config

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls", namespace="users")),
-    path("api/cars/", include("cars.urls", namespace="cars"))
+    path("api/", include("cars.urls", namespace="cars")),
+
+    # API
+    path('api/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
 ]
